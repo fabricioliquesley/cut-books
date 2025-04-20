51e87eecaedc08e13f46792a53f850cb210e9acb
Fabricio Liqueley <fabricioliquesley15@gmail.com>
2025-04-20 19:14:27 -0300

    connect all services of application

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
+import path from "node:path";
+
 import { loadCutSettings } from "./services/loadCutSettings";
 import { createBookFolder } from "./services/createBookFolder";
+import { loadHomeDir } from "./lib/loadHomeDir";
+import { cutBook } from "./services/cutBook";
 
 function main() {
   const cutSettings = loadCutSettings();
 
   if (!cutSettings) return;
 
-  createBookFolder(cutSettings[0].books[0].title.toLowerCase());
+  cutSettings.forEach((file) => {
+    const filePath = path.join(loadHomeDir(), "cutBook", file.filename);
 
-  console.log(cutSettings);
+    file.books.forEach(async (book) => {
+      const range = book.chapters
+        .map((chapter) => chapter.split("-").map(num => +num));
+      
+      const folderPath = createBookFolder(book.title.toLowerCase());
+
+      if (!folderPath) return;
+
+      await cutBook(filePath, folderPath, range);
+    })
+
+    // console.log(filePath);
+    // console.log(file.books)
+  })
+
+  // console.log(cutSettings);
 }
 
 main();
